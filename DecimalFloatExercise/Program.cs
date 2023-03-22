4724bb8a6c6efca6034e180116d5263c93b8fada
INTERNAL\rokas.cvirka <rokas.cvirka@visma.com>
2023-03-22 15:16:02 +0200

    Add project files.

---
+++ DecimalFloatExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DecimalFloatExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Create(@"C:\Users\rokas.cvirka\Documents\Numbers.txt");
+            File.CreateText(@"C:\Users\rokas.cvirka\Documents\Numbers.txt");
+            Random random = new Random();
+            random.Range();
+            Console.WriteLine(rn);
+        }
+    }
+}
